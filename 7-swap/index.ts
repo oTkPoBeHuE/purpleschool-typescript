442e2c61517741734a1c6674f6493ab12aae9a6d
oTkPoBeHuE <i.pakhotin@bars-open.ru>
2023-09-03 18:20:00 +0300

    Мелкие доработки

--- 7-swap/index.ts
+++ 7-swap/index.ts
@@ -1,13 +1,13 @@
-const obj3= {
-    a: '1',
-    b: '2'
-} satisfies Record<'a' | 'b', '1' | '2'>
-
 function swapKeysAndValues<T extends  object>(obj: T):{
     [K in keyof T as T[K]]: K;
 } {
     return Object.fromEntries(Object.entries(obj).map(arr => arr.reverse()))
 }
 
-const res = swapKeysAndValues(obj3)
+const obj= {
+    a: '1',
+    b: '2'
+} satisfies Record<'a' | 'b', '1' | '2'>
+
+const res = swapKeysAndValues(obj)
 console.log(res)
